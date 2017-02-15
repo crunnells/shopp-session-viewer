13990ec5e7dcca3299820c73395820cb0d9d40c6
Chris Runnells <chris@midpacificdesign.com>
2017-02-14 16:16:55 -1000

    Moved repo

--- shopp-session-tracker.php
+++ shopp-session-tracker.php
@@ -1,15 +1,18 @@
 <?php
 /*
 Plugin Name: Shopp Session Tracker
-Plugin URI: 
+Plugin URI:
 Description: Allows you to browse through Shopp's customer session data.
-Version: 0.2
+Version: 0.2.1
 Author: Chris Runnells
 Author URI: http://chrisrunnells.com
 */
 
 /*
 Version History:
+
+0.2.1 - 8/2/16
+Finally fixing display of cart items and errors in the list
 
 0.2 - 3/3/14
 Updating for Shopp 1.3
@@ -46,8 +49,8 @@ class ShoppSessionTracker {
 
 	function actions (){
 		$action = !empty($_REQUEST['action']) ? $_REQUEST['action'] : "";
-		
-		if("view" == $action){ 
+
+		if("view" == $action){
 			$this->view_session($_REQUEST['session']);
 		} else {
 			$this->list_sessions();
@@ -56,11 +59,14 @@ class ShoppSessionTracker {
 	}
 
 	function list_sessions(){
-		global $Shopp;
+		// global $Shopp;
 		$shopping_table = ShoppDatabaseObject::tablename('shopping');
-	   
+		// $db = DB::instance();
+        // $prefix = $db->table_prefix;
+		// $shopping_table = $prefix . "shopp_shopping";
+
 		$query = "SELECT * FROM $shopping_table ORDER BY modified DESC";
-		$results = sDB::query($query);
+		$results = sDB::query($query, 'array');
 
 ?>
 <style>
@@ -77,22 +83,43 @@ th {
 
 }
 tr:hover {
-	background-color: #eee;
+	background-color: #fff;
 }
 tr.you {
 	background-color: #ff0;
 }
+tr.viewed td.viewed {
+	background-color: #ccf;
+	color: #006;
+}
+tr.errors td.errors {
+	background-color: #fcc;
+	color: #c00;
+}
+tr.cartitems td.cartitems {
+	background-color: #cfc;
+	color: #060;
+}
+
+td.cartitems,
+td.errors,
+td.viewed {
+	text-align: center;
+	font-weight: bold;
+}
 </style>
 <h2>List Sessions</h2>
-<table>
+<table class="widefat">
   <thead>
     <tr>
       <th>Session ID</th>
       <th>Customer</th>
       <th>IP</th>
+      <th>Viewed Items</th>
 	  <th>Items in Cart</th>
       <th>Created</th>
       <th>Modified</th>
+      <th>Errors</th>
 	</tr>
   </thead>
   <tbody>
@@ -109,19 +136,38 @@ tr.you {
 				$customer = "";
 			}
 
-			// $cartitems = is_array($Session->ShoppCart->contents) ? count($Session->ShoppCart->contents) : 0;
-			
-			$class = ($_SERVER["REMOTE_ADDR"] == $result->ip) ? ' class="you"' : '';
+			$errors = count($Session->notices);
+			$cartitems = $Session->ShoppCart->count();
+			$viewed = count($Session->viewed);
+
+			$class = array();
+
+			if ($errors > 0){
+				$class[] = "errors";
+			}
+			if ($cartitems > 0){
+				$class[] = "cartitems";
+			}
+			if ($viewed > 0){
+				$class[] = "viewed";
+			}
+			if ($_SERVER["REMOTE_ADDR"] == $result->ip) {
+				$class[] = "you";
+			}
+
+			$classes = implode(" ", $class);
 ?>
-    <tr<?php echo $class; ?>>
+    <tr class="<?php echo $classes; ?>">
 		<td><a href="admin.php?page=<?php echo $this->sst_slug; ?>&action=view&session=<?php echo $result->session; ?>"><?php echo $result->session; ?></a></td>
 		<td><?php echo $customer; ?></td>
 		<td><?php echo $result->ip; ?></td>
-		<td><?php // echo $cartitems; ?></td>
+		<td class="viewed"><?php echo $viewed; ?></td>
+		<td class="cartitems"><?php echo $cartitems; ?></td>
 		<td><?php echo $this->nicetime($result->created); ?></td>
 		<td><?php echo $this->nicetime($result->modified); ?></td>
+		<td class="errors"><?php echo $errors; ?></td>
     </tr>
-<?php 
+<?php
 		}
 
 ?>
@@ -129,36 +175,36 @@ tr.you {
 </table>
 <?php
    } // end list_sessions();
-   
+
    function view_session($session){
 
-		global $Shopp;
+		// global $Shopp;
 		$shopping_table = ShoppDatabaseObject::tablename('shopping');
 
 		$query = "SELECT * FROM $shopping_table WHERE session = '$session'";
 		$results = sDB::query($query);
-		
+
 		$Session = unserialize($results->data);
 
 ?>
 <style type="text/css">
 .big {
-	width:90%; 
-	height:400px; 
-	overflow:scroll; 
+	width:90%;
+	height:400px;
+	overflow:scroll;
 	border:1px solid #ccc;
 }
 .small {
-	width:90%; 
-	height:200px; 
-	overflow:scroll; 
-	border:1px solid #ccc;	
+	width:90%;
+	height:200px;
+	overflow:scroll;
+	border:1px solid #ccc;
 }
 </style>
 <h1>Session ID: <?php echo $session; ?></h1>
 
 <h2>Errors</h2>
-<?php $this->errors($Session->errors); ?>
+<?php $this->errors($Session->notices); ?>
 
 <h2>Order</h2>
 <ul>
@@ -174,28 +220,34 @@ tr.you {
 		  </thead>
 		  <tbody>
 		    <tr>
-		      <td><?php echo $Session->Order->Customer->id; ?></td>
-		      <td><?php echo $Session->Order->Customer->firstname ." ". $Session->Order->Customer->lastname; ?>
-		      <td><?php echo $Session->Order->Customer->email; ?></td>
+		      <td><?php echo $Session->ShoppCustomer->id; ?></td>
+		      <td><?php echo $Session->ShoppCustomer->firstname ." ". $Session->ShoppCustomer->lastname; ?>
+		      <td><?php echo $Session->ShoppCustomer->email; ?></td>
 		    </tr>
 		  </tbody>
 		</table>
 	</li>
-	<li>Shipping Information</li>
-	<li>Billing Information</li>
+	<li>
+		<h3>Shipping Information</h3>
+		<pre class="big"><?php print_r($Session->ShippingAddress); ?>
+	</li>
+	<li>
+		<h3>Billing Information</h3>
+		<pre class="big"><?php print_r($Session->BillingAddress); ?>
+	</li>
 	<li>
 		<h3>Items in Cart</h3>
-<?php 
+<?php
 
 	$Cart = $Session->ShoppCart;
 echo "<ul>";
-	foreach($Cart->contents as $item){
+	foreach($Cart as $id => $item){
 		echo "<h4>". $item->name ."</h4>";
 		echo '<li>'. $this->cart_items($item) . '</li>';
 	}
 echo "</ul>";
 ?>
-		
+
 	</li>
 </ul>
 
@@ -215,11 +267,11 @@ echo "</ul>";
 </pre>
 
 <h2>Raw Session Data</h2>
-<pre class="big">
+<pre>
 <?php print_r(unserialize($results->data)); ?>
 </pre>
-<?php 
-   
+<?php
+
 	}
 
 	function promotions($promotions){
@@ -228,32 +280,36 @@ echo "</ul>";
 ?>
 <pre class="big">
 <?php print_r($promotions); ?>
-</pre>			
+</pre>
 <?php
 	}
 
 	function errors($errors){
 ?>
 <pre class="big">
-<?php print_r($errors); ?>
-</pre>			
-<?php	
+<?php
+foreach ($errors as $error) {
+	echo $error;
+}
+?>
+</pre>
+<?php
 	}
 
 	function cart_items($output){
 ?>
 <pre class="small">
 <?php print_r($output); ?>
-</pre>			
-<?php	
+</pre>
+<?php
 	}
 
 	function customer($customer){
 ?>
 <pre class="big">
 <?php print_r($customer); ?>
-</pre>			
-<?php		
+</pre>
+<?php
 	}
 
 	function viewed_items($viewed) {
@@ -265,10 +321,10 @@ echo "</ul>";
 
 	function nicetime($date){
 
-	    if(empty($date)) {
+	    if ( empty( $date ) ) {
 	        return "No date provided";
 	    }
-	   
+
 	    $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
 	    $lengths = array("60","60","24","7","4.35","12","10");
 
@@ -279,12 +335,12 @@ echo "</ul>";
 		$unix_date = strtotime($date);
 
 		// check validity of date
-		if(empty($unix_date)) {   
+		if(empty($unix_date)) {
 			return "Bad date";
 		}
 
 	    // is it future date or past date
-		if($now > $unix_date) {   
+		if ( $now > $unix_date ) {
 	        $difference = $now - $unix_date;
 	        $tense = "ago";
 
@@ -293,7 +349,7 @@ echo "</ul>";
 	        $tense = "from now";
 	    }
 
-	    for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+	    for ( $j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++ ) {
 	        $difference /= $lengths[$j];
 	    }
 
@@ -307,5 +363,5 @@ echo "</ul>";
 	}
 
 } // end Class
- 
+
 
